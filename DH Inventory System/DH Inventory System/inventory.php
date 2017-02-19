3226d4aa88f6e5c87fa69423d96c0904b987f114
Kreeztian <KreezyKreez@yahoo.com>
2017-02-19 14:29:25 +0800

    updated Login and inv.php

--- DH Inventory System/DH Inventory System/inventory.php
+++ DH Inventory System/DH Inventory System/inventory.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
 
- <?php
-	session_start();
-	
-	if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
-		header('Location: Login.php');
-	}
-
-?>
-
 <html lang="en">
 
   <head>
@@ -16,19 +7,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Inventory</title>
+		<?php include('dbcon.php'); ?>
+<?php 
+session_start();
+if (isset($_SESSION['id'])){
+header('Location: inventory.php');
+$session_id = $_SESSION['id'];
+$session_query = $conn->query("select * from users where userName = '$session_id'");
+$user_row = $session_query->fetch();
+ if (!isset($_SESSION['id']) || $_SESSION['id'] == false) {
+		session_destroy();
+		header('Location: index.php');
+ }
+}
+?>
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="logo.jpg">
     <link rel="stylesheet" type ="text/css" href="css/bootstrap.css">
 	<script src="js/bootstrap.js"></script>
-	<link rel="shortcut icon" href="logo.jpg">
+	<?php require('dbcon.php'); ?>
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
   
-  <body>
-	<div class="productHolder">
-		<nav class="navbar navbar-inverse navbar-static-top">
+  <body >
+	<div class="productHolder" >
+		<nav class="navbar navbar-inverse navbar-static-top" >
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -41,7 +47,7 @@
 				</div>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right" id="categories">
-						<li><a href="inventory.html">Inventory</a></li>
+						<li><a href="inventory.php">Inventory</a></li>
 						<li><a href="product.html">Products</a></li>
 						<li><a href="incoming.html">Incoming</a></li>
 						<li><a href="outgoing.html">Outgoing</a></li>
@@ -50,29 +56,19 @@
 				</div>
 			</div>
 		</nav>
-	</div>	
-
-	<div class="product">	
-	<h1 id="headers">Inventory</h1>
-		<form>
-			<input type="search" name="searchbox" placeholder="Search" id="search">
-		</form>
-		<br>
-		<select style="width: 200px">
-		  <option value="" disabled selected hidden>Search By</option>
-		  <option value="searchby">Sample 1</option>
-		  <option value="searchby">Sample 2</option>
-		  <option value="searchby">Sample 3</option>
-		</select>
+	</div>
+		<div class="product">
+			<h1 id="headers">Inventory</h1>
 		
-		<select id="prodSort" style="width: 200px">
-		  <option value="" disabled selected hidden>Sort By</option>
-		  <option value="searchby">Sample 1</option>
-		  <option value="searchby">Sample 2</option>
-		  <option value="searchby">Sample 3</option>
-		</select>
-		
+			<select id="prodSort">
+			  <option value="" disabled selected hidden>Sort By</option>
+			  <option value="searchby">Sample 1</option>
+			  <option value="searchby">Sample 2</option>
+			  <option value="searchby">Sample 3</option>
+			</select>
+			
 		<div class="prodTable">
+			<br>
 			<table border="1" id="table">
 				<tr>
 				<th style="text-align:center">Item</th>
@@ -80,50 +76,41 @@
 				<th style="text-align:center">Serial Code</th>
 				<th style="text-align:center">Quantity</th>
 				<th style="text-align:center">Quantity Threshold</th>
-				<th></th>
 
 				</tr>
 				<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td><button type="button">Edit</button></td>
-				<td><button type="button">Remove</button></td>
+				<td>Item 1</td>
+				<td>Brand X</td>
+				<td>SE10234</td>
+				<td>5</td>
+				<td>2</td>
 				</tr>
 				<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td><button type="button">Edit</button></td>
-				<td><button type="button">Remove</button></td>
+				<td>Item 1</td>
+				<td>Brand X</td>
+				<td>SE10234</td>
+				<td>5</td>
+				<td>2</td>
 				</tr>
 				<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td><button type="button">Edit</button></td>
-				<td><button type="button">Remove</button></td>
+				<td>Item 1</td>
+				<td>Brand X</td>
+				<td>SE10234</td>
+				<td>5</td>
+				<td>2</td>
 				</tr>
 				<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td><button type="button">Edit</button></td>
-				<td><button type="button">Remove</button></td>
+				<td>Item 1</td>
+				<td>Brand X</td>
+				<td>SE10234</td>
+				<td>5</td>
+				<td>2</td>
 				</tr>
 			</table>
 		</div>	
 		
-		<form action="alert.html" target="_blank">
-			<input type="submit" value="Check for Understocked Products">
-		</form>
-
-		<form action="addInventory.html" target="_blank">
-			<input id="myBtn" type="submit" value="Add Stock">
+		<form action="inventory.html" target="_blank">
+			<input type="submit" value="Check for Understocked Products" class="btn btn-default btnAlign">
 		</form>
 	</div>
 	<nav class="navbar navbar-inverse navbar-fixed-bottom">
@@ -138,7 +125,10 @@
 			</div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-right" id="logout">
-				<li><a href="login.html">Logout</a></li>
+				<li><a href="Logout.php">Logout</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-left" id="report">
+				<li><a href="report.html">Print Report</a></li>
 			</ul>
 		</div>
 		</div>
