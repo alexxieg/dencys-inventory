cf4d880090adc3af0da106986c37ea2c5a671a52
Kreeztian <KreezyKreez@yahoo.com>
2017-03-21 19:13:51 +0800

    updated login for userprod.

--- DH Inventory System/DH Inventory System/userproduct.php
+++ DH Inventory System/DH Inventory System/userproduct.php
@@ -12,7 +12,7 @@
 		<?php 
 				session_start();
 				$role = $_SESSION['sess_role'];
-				if (!isset($_SESSION['id']) && $role!="user") {
+				if (!isset($_SESSION['id']) || $role!="user") {
 					header('Location: index.php');
 				}
 					$session_id = $_SESSION['id'];
