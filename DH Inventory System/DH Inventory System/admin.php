f1687b93e285d059d4fe08b6a5bd522961efad62
Kreeztian <KreezyKreez@yahoo.com>
2017-03-21 19:15:58 +0800

    updated login for admin.

--- DH Inventory System/DH Inventory System/admin.php
+++ DH Inventory System/DH Inventory System/admin.php
@@ -12,6 +12,19 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		
+		<?php include('dbcon.php'); ?>
+		<?php 
+			session_start();
+			$role = $_SESSION['sess_role'];
+			if (!isset($_SESSION['id']) || $role!="admin") {
+				header('Location: index.php');
+			}
+				$session_id = $_SESSION['id'];
+				$session_query = $conn->query("select * from users where userName = '$session_id'");
+				$user_row = $session_query->fetch();
+		?>
+		
 	</head>
 	  
 	<body>
