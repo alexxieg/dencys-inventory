fb5f26c45b1cb8b68b32e97387bfaf58d656e14b
Kreeztian <KreezyKreez@yahoo.com>
2017-03-27 22:25:54 +0800

    merge

--- DH Inventory System/incoming.php
+++ DH Inventory System/incoming.php
@@ -15,8 +15,8 @@
 		<script src="js/bootstrap.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="sweetalert2/dist/sweetalert2.min.js"></script>
-		<link rel="stylesheet" href="sweetalert2/dist/sweetalert2.min.css">
+		<script src="alertboxes/sweetalert2.min.js"></script>
+		<link rel="stylesheet" href="alertboxes/sweetalert2.min.css">
 		
 		<?php include('dbcon.php'); ?>
 		<?php 
