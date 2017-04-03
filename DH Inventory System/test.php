bb136bd31b9b31a84bb9a02d44708d2b2bf5de40
Alex <alelexxie@gmail.com>
2017-04-03 14:03:32 +0800

    Edited test page

--- DH Inventory System/test.php
+++ DH Inventory System/test.php
@@ -88,22 +88,24 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
+            <li class="active"><a href="inventory.php">Inventory<span class="sr-only">(current)</span></a></li>
+            <li><a href="incoming.php">Incoming</a></li>
+            <li><a href="outgoing.php">Outgoing</a></li>
+            <li><a href="returns.php">Returns</a></li>
           </ul>
           <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
+            <li><a href="">Reports</a></li>
+            <li><a href="branchReport.php">Branch Report</a></li>
           </ul>
           <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
+            <li><a href="">Manage</a></li>
+            <li><a href="accounts.php">Accounts</a></li>
+            <li><a href="branch.php">Branches</a></li>
+			<li><a href="employees.php">Employees</a></li>
+			<li><a href="product.php">Products</a></li>
+			<li><a href="brand.php">Product Brands</a></li>
+			<li><a href="category.php">Product Categories</a></li>
+			
           </ul>
         </div>
         <!-- PHP code for fetching the data-->
