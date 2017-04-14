8187d7f86d97d5429fba1de867a6358ae256e6e3
r-den <guillermoarden>
2017-04-14 16:06:29 +0900

    aaaaaaaaaaadded custom receiptno generator for return functions

--- DH Inventory System/functionalities/addReturnSupplier.php
+++ DH Inventory System/functionalities/addReturnSupplier.php
@@ -2,6 +2,17 @@
 	if (isset($_POST["addRet"])){
 				
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$recRetrieve = $conn->prepare("SELECT receiptNo FROM returns");
+		$recRetrieve->execute();
+		$recs = $recRetrieve->fetchAll();
+		$recBase = 00001;
+		$recNo = 'RET-SUP-' . str_pad((string)$recBase,5,0,STR_PAD_LEFT);
+		foreach ($nums AS $list):
+			if ($recNo == $list["receiptNo"]){
+				$numBase = $recBase + 1;
+				$recNo = 'RET-SUP-' . str_pad((string)$recBase,5,0,STR_PAD_LEFT);
+			}
+		endforeach;
 								
 		$prod = $_POST['prodItem'];
 							
@@ -9,8 +20,8 @@
 		$prod2 = $prod1->fetch(PDO::FETCH_ASSOC);
 		$prod3 = $prod2['prodA'];
 					
-		$sql = "INSERT INTO returns (returnDate, returnQty, returnType, returnRemark, prodID)
-				VALUES (CURDATE(),'".$_POST['retQty']."','Supplier Return','".$_POST['retRemarks']."','$prod3')";
+		$sql = "INSERT INTO returns (returnDate, returnQty, returnType, returnRemark, receiptNo, prodID)
+				VALUES (CURDATE(),'".$_POST['retQty']."','Supplier Return','".$_POST['retRemarks']."','$recNo','$prod3')";
 		$conn->exec($sql);
 		echo "<meta http-equiv='refresh' content='0'>";
 	}   
--- DH Inventory System/functionalities/addReturnWarehouse.php
+++ DH Inventory System/functionalities/addReturnWarehouse.php
@@ -2,7 +2,19 @@
 	if (isset($_POST["addRet"])){
 				
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-								
+		
+		$recRetrieve = $conn->prepare("SELECT receiptNo FROM returns");
+		$recRetrieve->execute();
+		$recs = $recRetrieve->fetchAll();
+		$recBase = 00001;
+		$recNo = 'RET-WHS-' . str_pad((string)$recBase,5,0,STR_PAD_LEFT);
+		foreach ($nums AS $list):
+			if ($recNo == $list["receiptNo"]){
+				$numBase = $recBase + 1;
+				$recNo = 'RET-WHS-' . str_pad((string)$recBase,5,0,STR_PAD_LEFT);
+			}
+		endforeach;
+		
 		$prod = $_POST['prodItem'];
 		$branch = $_POST['branchRet'];
 							
@@ -14,8 +26,8 @@
 		$branch2 = $branch1->fetch(PDO::FETCH_ASSOC);
 		$branch3 = $branch2['branchA'];
 					
-		$sql = "INSERT INTO returns (returnDate, returnQty, returnType, returnRemark, prodID, branchID)
-				VALUES (CURDATE(),'".$_POST['retQty']."','Warehouse Return','".$_POST['retRemarks']."','$prod3','$branch3')";
+		$sql = "INSERT INTO returns (returnDate, returnQty, returnType, returnRemark, prodID, receiptNo ,branchID)
+				VALUES (CURDATE(),'".$_POST['retQty']."','Warehouse Return','".$_POST['retRemarks']."','$prod3','$recNo','$branch3')";
 		$conn->exec($sql);
 		echo "<meta http-equiv='refresh' content='0'>";
 	}   
