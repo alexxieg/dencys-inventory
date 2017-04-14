0bacd5fd10c56dbea9bc591b8f36502c4f0feb54
r-den <guillermoarden>
2017-04-14 15:04:06 +0900

    added custom-made ponumber

--- DH Inventory System/functionalities/addPO.php
+++ DH Inventory System/functionalities/addPO.php
@@ -3,7 +3,17 @@
     if (isset($_POST['submit'])) {
 
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
+		$numretrieve = $conn->prepare("SELECT poNumber FROM purchaseorders");
+		$numretrieve->execute();
+		$nums = $numretrieve->fetchAll();
+		$numBase = 00001;
+		$prod = 'PO-' . str_pad((string)$numBase,5,0,STR_PAD_LEFT);
+		foreach ($nums AS $list):
+			if ($prod == $list["poNumber"]){
+				$numBase = $numBase + 1;
+				$prod = 'PO-' . str_pad((string)$numBase,5,0,STR_PAD_LEFT);
+			}
+		endforeach; 
 		   for ($index = 0; $index < count($prodTem); $index++) {
 
 				$prodItem = $_POST['prodItem'][$index];
@@ -12,8 +22,8 @@
 				$prod1 = $conn->query("SELECT prodID AS prodA FROM product WHERE prodName = '$prodItem'");
 				$prod2 = $prod1->fetch(PDO::FETCH_ASSOC);
 				$prod3 = $prod2['prodA'];
-				$sql = "INSERT INTO purchaseorders (qtyOrder, poDate, supplier, prodID)
-				VALUES ('$qty',CURDATE(),'".$_POST['supplier']."','$prod3')";
+				$sql = "INSERT INTO purchaseorders (qtyOrder, poDate, poNumber, supplier, prodID)
+				VALUES ('$qty',CURDATE(),'$prod','".$_POST['supplier']."','$prod3')";
 				$result = $conn->query($sql); 
 
 				echo "<meta http-equiv='refresh' content='0'>";
