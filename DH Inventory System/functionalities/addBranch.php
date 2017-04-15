3cf51d082f979947c6307e540380b2b2f0dcdb25
CheeseLegs <cheeseylegs@gmail.com>
2017-04-15 20:22:06 +0800

    Fixed Add functionality

--- DH Inventory System/functionalities/addBranch.php
+++ DH Inventory System/functionalities/addBranch.php
@@ -3,8 +3,8 @@
 			
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			 
-		$sql = "INSERT INTO branch (branchID, branch)
-				VALUES ('".$_POST['branchID']."','".$_POST['branch']."')";
+		$sql = "INSERT INTO branch (branchID, location, branchName, status)
+				VALUES ('".$_POST['branchID']."','".$_POST['location']."','".$_POST['branchName']."','Active')";
 		$conn->exec($sql);
 		echo "<meta http-equiv='refresh' content='0'>";
 	}    
