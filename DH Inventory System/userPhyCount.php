e2aa83848cf4bbbdb17fc0bdd118b19a32cbedca
Kreeztian <KreezyKreez@yahoo.com>
2017-04-16 18:13:32 +0800

    Fixed login function.

--- DH Inventory System/userPhyCount.php
+++ DH Inventory System/userPhyCount.php
@@ -28,15 +28,13 @@
 				
 		<?php 
 			session_start();
-			if (isset($_SESSION['id'])){
-				$session_id = $_SESSION['id'];
-				$session_query = $conn->query("select * from users where userName = '$session_id'");
-				$user_row = $session_query->fetch();
-				if (!isset($_SESSION['id']) || $_SESSION['id'] == false) {
-					session_destroy();
-					header('Location: index.php');
-				}
+			$role = $_SESSION['sess_role'];
+			if (!isset($_SESSION['id']) || $role!="user") {
+				header('Location: index.php');
 			}
+			$session_id = $_SESSION['id'];
+			$session_query = $conn->query("select * from users where userName = '$session_id'");
+			$user_row = $session_query->fetch();
 		?>
 	</head>
 	  
