80956914fc9461fed81fef62aabd3177b439fc2a
r-den <guillermoarden>
2017-04-18 02:16:49 +0900

    clicking on dencys hardware now goes to inventory

--- DH Inventory System/functionalities/addDefective.php
+++ DH Inventory System/functionalities/addDefective.php
@@ -50,7 +50,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Dency's Hardware and General Merchandise</a>
+					<a class="navbar-brand" href="../inventory.php">Dency's Hardware and General Merchandise</a>
 				</div>
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav navbar-right">
