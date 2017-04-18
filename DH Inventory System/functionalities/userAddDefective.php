3883ad9060b923acadbf24328485abbc1be422c5
Kreeztian <KreezyKreez@yahoo.com>
2017-04-18 23:54:58 +0800

    Fixed sidebar navigation and paths.

--- DH Inventory System/functionalities/userAddDefective.php
+++ DH Inventory System/functionalities/userAddDefective.php
@@ -68,7 +68,7 @@
 						<div id="sidebarLogo"><img src="../logo.png" alt=""/></div>
 						<li class="active"><a href="#"data-toggle="collapse" data-target="#inventory"><i class="glyphicon glyphicon-list-alt"></i> Inventory  <span class="sr-only">(current)</span><i class="glyphicon glyphicon-menu-down" id="dropDownArrow"></i></a>
 							<ul class="list-unstyled collapse" id="inventory">
-								<li><a href="userinventory.php"><i class="glyphicon glyphicon-list"></i> Current Inventory</a></li>
+								<li><a href="../userinventory.php"><i class="glyphicon glyphicon-list"></i> Current Inventory</a></li>
 								<li><a href="functionalities/userAddDefective.php"><i class="glyphicon glyphicon-list"></i> Add Defectives</a></li>
 							</ul>
 						</li>
