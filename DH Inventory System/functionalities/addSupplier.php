5973ceec73534ec7746b1be5b156f29df653e71e
Alex <alelexxie@gmail.com>
2017-05-05 07:30:59 +0800

    Added supplier page. Not yet integrated in the sidebar.

---
+++ DH Inventory System/functionalities/addSupplier.php
@@ -0,0 +1,29 @@
+<?php
+	if (isset($_POST["addSup"])){
+	
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+		$supName = $_POST['supName'];
+
+		$query = $conn->prepare("Select * FROM suppliers WHERE supplier_name = '$supName'");
+		$count = $query->execute();
+		$row = $query->fetch();
+
+		if ($query->rowCount() > 0){
+			echo '<script language="javascript">';
+			echo 'swal(
+				  "Error!",
+				  "Supplier Already Exists, New Employee Has Not been Added",
+				  "error");';
+			echo '$("#myModal").modal("show");';
+			echo 'document.getElementById("adduser").style.borderColor = "red";';
+			echo '</script>';
+		} else {
+				 
+				$sql = "INSERT INTO suppliers (supplier_name)
+				VALUES ('$supName')";
+				$conn->exec($sql);
+			echo "<meta http-equiv='refresh' content='0'>";
+		}
+	}			
+?>
