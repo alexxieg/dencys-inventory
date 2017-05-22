8ecc0eb8938e818c69f46863fd2539be967ec590
CheeseLegs <cheeseylegs@gmail.com>
2017-05-22 23:56:31 +0800

    autocomplete for user product issuance

--- DH Inventory System/userProdIssuance.php
+++ DH Inventory System/userProdIssuance.php
@@ -25,6 +25,11 @@
 		<script src="alertboxes/sweetalert2.min.js"></script>
 		<link rel="stylesheet" href="alertboxes/sweetalert2.min.css">
 		
+		<!-- Autocomplete Script -->
+		<link rel="stylesheet" href="css/jquery-ui.css">
+		<script src="js/jquery-1.9.1.js"></script>
+		<script src="js/jquery-ui.js"></script>
+		
 		<!-- Datatables CSS and JS Files -->
 		<script src="datatables/media/js/jquery.dataTables.min.js"></script>
 		<script src="datatables/media/js/dataTables.bootstrap.min.js"></script>
@@ -60,6 +65,26 @@
 					} ]
 				} );
 			} );
+
+		</script>
+		
+		<script>
+		  $(function() {
+			$('.thisProduct').autocomplete({
+				minLength:2,
+				source: "search.php"
+			});
+		  });
+
+		</script>
+		
+		<script>
+		  $(function() {
+			$('#supplier').autocomplete({
+				minLength:2,
+				source: "searchSup.php"
+			});
+		  });
 
 		</script>
 		
@@ -286,16 +311,9 @@
 															<td><input type="checkbox" name="chk"></TD>
 															<td><input type="hidden" value="1" name="num" id="orderdata">1</TD>
 															<td>	
-																<?php
-																	$query = $conn->prepare("SELECT prodName FROM product INNER JOIN inventory ON product.prodID = inventory.prodID WHERE inventory.qty != 0 OR NOT NULL");
-																	$query->execute();
-																	$res = $query->fetchAll();
-																?>
-																<select class="form-control" id="addItem" name="prodItem[]">
-																<?php foreach ($res as $row): ?>
-																	<option><?=$row["prodName"]?></option>
-																<?php endforeach ?>
-															</select> 
+																<div class="ui-widget">
+																	<input class="thisProduct" name="prodItem">
+																</div>
 															</td>
 																	
 															<td>
