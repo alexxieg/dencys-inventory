54de20a8920549438e20269de7d002f601644024
CheeseLegs <cheeseylegs@gmail.com>
2017-05-22 23:42:15 +0800

    fixed user side for product deliveries

---
+++ DH Inventory System/functionalities/search.php
@@ -0,0 +1,22 @@
+<?php
+
+include("dbcon.php"); /* ESTABLISH CONNECTION IN THIS FILE; MAKE SURE THAT IT IS mysqli_* */
+
+$stmt = $conn->prepare('SELECT prodName FROM product WHERE prodName LIKE :term'); /* START PREPARED STATEMENT */
+$stmt->execute(array('term' => '%'.$_GET['term'].'%'));
+
+//return json data
+try {
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	while($row = $stmt->fetch()) {
+	  $return_arr[] =  $row['prodName'];
+	}
+	
+} catch(PDOException $e) {
+	echo 'ERROR: ' . $e->getMessage();
+}
+    /* Toss back results as json encoded array. */
+    echo json_encode($return_arr);
+
+?>
