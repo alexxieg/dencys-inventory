05f4c23fc09eb7532ab1c1fe8524ae9c8a22cd4e
Alysmacanas <alysmatiasmacanas@gmail.com>
2017-05-26 14:54:22 +0800

    Added print function and page length for user

--- DH Inventory System/functionalities/userViewProdIssuance.php
+++ DH Inventory System/functionalities/userViewProdIssuance.php
@@ -30,14 +30,59 @@
 		<!-- Datatables CSS and JS Files -->
 		<script src="../datatables/media/js/jquery.dataTables.min.js"></script>
 		<script src="../datatables/media/js/dataTables.bootstrap.min.js"></script>
-		<link href="../datatables/media/css/dataTables.bootstrap.min.css" rel="stylesheet">	
-		<link href="..datatables/media/css/jquery.dataTables.min.css" rel="stylesheet">
-		
+		<script src="../datatables/Buttons/js/dataTables.buttons.min.js"></script>
+		<script src="../datatables/Buttons/js/buttons.bootstrap.min.js"></script>
+		<script src="../datatables/media/js/buttons.html5.min.js"></script>
+		<script src="../datatables/Buttons/js/buttons.print.min.js"></script>
+		<script src="../datatables/Buttons/js/buttons.colVis.min.js"></script>
+
+		<link href="../datatables/media/css/dataTables.bootstrap.min.css"rel="stylesheet">
+		<link href="../datatables/media/css/dataTables.bootstrap.css" rel="stylesheet">
+		<link href="../datatables/Buttons/css/buttons.bootstrap.min.css" rel="stylesheet">		
+
+        <link href="../datatables/Buttons/css/buttons.dataTables.min.css"rel="stylesheet">
+        <script src="../datatables/Buttons/js/buttons.print.min.js"></script>
+		<script src="../datatables/Buttons/js/buttons.colVis.min.js"></script>
+
 		<!-- Datatables Script -->
 		<script>
-			$(document).ready(function(){
-				$('#myTable').dataTable();
-			});
+			$(document).ready(function() {
+                $('#myTable').DataTable( {
+                    dom: 'Bfrtip',
+					lengthMenu: [
+						[ 10, 25, 50, 100, -1 ],
+						[ '10 rows', '25 rows', '50 rows', '100 rows', 'Show all' ]
+					],
+                    buttons: [
+                        {
+                            title: 'Dencys Hardware and General Merchandise', 
+							message: 'Product Issuance Details', 
+							customize: function ( win ) {
+                                $(win.document.body)
+                                    .css( 'font-size', '10pt' )
+                                    .prepend(
+                                        '<img src="http://localhost/dencys/DH%20Inventory%20System/logo.png" style="position:relative; bottom:5%; float: right; height:120px; width:120px;" />'
+                                    );
+
+                                $(win.document.body).find( 'table' )
+                                    .addClass( 'compact' )
+                                    .css( 'font-size', 'inherit' );
+                            },
+                                extend: 'print',
+                                exportOptions: {
+                                columns: ':visible'
+                                }
+                        },
+							'colvis','pageLength',
+
+                    ],
+                        columnDefs: [{
+                            targets: -1,
+                            visible: true
+                            
+                        }]
+                } );
+            } );		
 		</script>
 		
 		<!-- Database Connection -->
@@ -156,7 +201,6 @@
 								</button>
 							</a>
 							
-							<input type="button" class="btn btn-default" id="modButt" onclick="window.print()" value="PRINT TABLE" />
 							<hr>
 	
 							<table class="table table-striped table-bordered">
@@ -184,7 +228,6 @@
 							</div>
 						</div>
 						
-						<div id="printThisTable" name="printThisTable">
 							<!-- Table Display for Outgoing Entries -->
 							<table id="myTable" class="table table-hover table-bordered dataTable" cellspacing="0" width="100%" role="grid" aria-describedby="myTable_info" style="width: 100%;">
 								<thead>		
@@ -217,7 +260,6 @@
 					</div>
 				</div>
 			</div>
-		</div>
 	
   </body>
 </html>
