ced79d18931a9f9e748e5781e4fb85cd1755df2d
Kreeztian <kreezykreez@yahoo.com>
2017-05-31 14:25:38 +0800

    Fixed login bug for user supplier returns

--- DH Inventory System/userreturnSupplier.php
+++ DH Inventory System/userreturnSupplier.php
@@ -6,6 +6,21 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<title>Return to Supplier</title>
+					
+		<!-- Database Connection -->
+		<?php include('dbcon.php'); ?>
+		
+		<!-- Login Session -->
+		<?php 
+			session_start();
+			$role = $_SESSION['sess_role'];
+			if (!isset($_SESSION['id']) || $role!="user") {
+				header('Location: index.php');
+			}
+			$session_id = $_SESSION['id'];
+			$session_query = $conn->query("select * from users where userName = '$session_id'");
+			$user_row = $session_query->fetch();
+		?>
 		
 		<!-- Bootstrap core CSS -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
@@ -82,21 +97,7 @@
                 } );
             } );		
 		</script>
-		
-		<!-- Database Connection -->
-		<?php include('dbcon.php'); ?>
-		
-		<!-- Login Session -->
-		<?php 
-			session_start();
-			$role = $_SESSION['sess_role'];
-			if (!isset($_SESSION['id']) || $role!="user") {
-				header('Location: index.php');
-			}
-			$session_id = $_SESSION['id'];
-			$session_query = $conn->query("select * from users where userName = '$session_id'");
-			$user_row = $session_query->fetch();
-		?>
+
 	</head>
 
 	<body>
