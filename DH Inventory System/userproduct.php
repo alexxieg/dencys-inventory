bd669b740a64a474dc622616812306744c23166f
Kreeztian <kreezykreez@yahoo.com>
2017-05-31 14:41:10 +0800

    Fixed login bug for user product

--- DH Inventory System/userproduct.php
+++ DH Inventory System/userproduct.php
@@ -6,6 +6,21 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<title>Incoming Products</title>
+		
+		<!-- Database Connection -->
+		<?php include('dbcon.php'); ?>
+	
+		<!-- Login Session -->
+		<?php 
+			session_start();
+			$role = $_SESSION['sess_role'];
+			if (!isset($_SESSION['id']) || $role!="user") {
+				header('Location: index.php');
+			}
+			$session_id = $_SESSION['id'];
+			$session_query = $conn->query("select * from users where userName = '$session_id'");
+			$user_row = $session_query->fetch();
+		?>
 
 		<!-- Bootstrap core CSS -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
@@ -83,20 +98,6 @@
             } );		
 		</script>
 			
-		<!-- Database Connection -->
-		<?php include('dbcon.php'); ?>
-	
-		<!-- Login Session -->
-		<?php 
-			session_start();
-			$role = $_SESSION['sess_role'];
-			if (!isset($_SESSION['id']) || $role!="user") {
-				header('Location: index.php');
-			}
-			$session_id = $_SESSION['id'];
-			$session_query = $conn->query("select * from users where userName = '$session_id'");
-			$user_row = $session_query->fetch();
-		?>
 	</head>
  
 	<body>
