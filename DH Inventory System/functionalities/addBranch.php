63ec12018689ec0c22515e8d8287e81714d68f32
Kreeztian <KreezyKreez@yahoo.com>
2017-06-04 06:26:38 +0800

    added branch validation for add Branch.

--- DH Inventory System/functionalities/addBranch.php
+++ DH Inventory System/functionalities/addBranch.php
@@ -2,10 +2,28 @@
 	if (isset($_POST["addBranch"])){
 			
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$branchName = $_POST['branchName'];
+	    $location = $_POST['location'];
+		$query = $conn->prepare("Select * FROM branch WHERE branchName = '$branchName' AND location = '$location'");
+		$count = $query->execute();
+		$row = $query->fetch();
+
+		if ($query->rowCount() > 0){
+			echo '<script language="javascript">';
+			echo 'swal(
+				  "Error!",
+				  "Branch Already Exists, New Branch Has Not been Added",
+				  "error");';
+			echo '$("#myModal").modal("show");';
+			echo 'document.getElementById("addBranchName").style.borderColor = "red";';
+			echo '</script>';
+		} else {
 			 
 		$sql = "INSERT INTO branch (location, branchName, status)
 				VALUES ('".$_POST['location']."','".$_POST['branchName']."','Active')";
 		$conn->exec($sql);
 		echo "<meta http-equiv='refresh' content='0'>";
+		}
 	}    
 ?>
--- DH Inventory System/js/branches.js
+++ DH Inventory System/js/branches.js
@@ -2,7 +2,17 @@ function validateForm() {
 	if(document.getElementById('addBranchName').value == "") {
 		swal({
 		title: "Warning!",
-		text: "Please Enter Branch ID.",
+		text: "Please Enter Branch Name.",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addBranchName').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addBranchName').value.length < 3){
+		swal({
+		title: "Warning!",
+		text: "The Branch Name should have more than 2 characters",
 		type: "warning",
 		confirmButtonText: "Ok"
 		});
@@ -12,7 +22,17 @@ function validateForm() {
 	if (document.getElementById('addBranchLoc').value == "") {
 		swal({
 		title: "Warning!",
-		text: "Please Enter Branch Name.",
+		text: "Please Enter Branch Location.",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addBranchLoc').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addBranchLoc').value.length < 3){
+		swal({
+		title: "Warning!",
+		text: "The Branch Location should have more than 2 characters",
 		type: "warning",
 		confirmButtonText: "Ok"
 		});
@@ -26,6 +46,60 @@ function validateForm() {
 	else {
 		swal({
 		title: "Adding of Branch Canceled",
+		type: "success"
+		});
+		return false;		
+	}
+}
+
+function validateForm2() {
+	if(document.getElementById('addEntry').value == "") {
+		swal({
+		title: "Warning!",
+		text: "Please Enter Branch Name.",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addEntry').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addEntry').value.length < 3){
+		swal({
+		title: "Warning!",
+		text: "The Branch Name should have more than 2 characters",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addEntry').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addEntrys').value == "") {
+		swal({
+		title: "Warning!",
+		text: "Please Enter Branch Location.",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addEntrys').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addEntrys').value.length < 3){
+		swal({
+		title: "Warning!",
+		text: "The Branch Location should have more than 2 characters",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addEntrys').style.borderColor = "red";
+		return false;
+	}
+	if(confirm('Are you sure you want to update this entry?')) {
+		alert("New Branch Successfully Added");
+		return true;	
+	}
+	else {
+		swal({
+		title: "Update Canceled",
 		type: "success"
 		});
 		return false;		
