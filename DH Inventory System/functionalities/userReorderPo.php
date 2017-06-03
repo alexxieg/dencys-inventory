c5be7bb01e1ef9a7b70d81cbd7d91d85d55132a0
Alex <alelexxie@gmail.com>
2017-06-03 13:35:31 +0800

    Edited header

--- DH Inventory System/functionalities/userReorderPo.php
+++ DH Inventory System/functionalities/userReorderPo.php
@@ -124,7 +124,7 @@
 				</div>
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav navbar-right">
-						<li id="adminhead"><a href="#">Admin |</a></li>
+						<li id="adminhead"><a href="#">User |</a></li>
 						<li><a href="Logout.php"><i class="glyphicon glyphicon-off"></i> LOGOUT</a></li>
 					</ul>
 				</div>
