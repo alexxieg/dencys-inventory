589608e2ef5c52084f880085a0736e9b44b6c3c9
Kreeztian <KreezyKreez@yahoo.com>
2017-06-05 04:46:19 +0800

    fixed path for validation in user edit out.

--- DH Inventory System/functionalities/userEditOut.php
+++ DH Inventory System/functionalities/userEditOut.php
@@ -37,6 +37,8 @@
 		<script src="../js/bootstrap.js"></script>
 		<script src="../js/jquery-3.2.0.min.js"></script>	
 		<script src="../js/bootstrap.min.js"></script>
+		<script src="../alertboxes/sweetalert2.min.js"></script>
+		<link rel="stylesheet" href="../alertboxes/sweetalert2.min.css">
 					
 		<!-- Autocomplete Script -->
 		<link rel="stylesheet" href="../css/jquery-ui.css">
