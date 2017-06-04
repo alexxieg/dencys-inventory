ce0a1c09ebff78f463849d3fe480ae810904f37a
Kreeztian <KreezyKreez@yahoo.com>
2017-06-05 01:46:02 +0800

    added edit brand validation.

--- DH Inventory System/js/brand.js
+++ DH Inventory System/js/brand.js
@@ -50,3 +50,56 @@ function validateForm() {
 		return false;		
 	}
 }
+
+function validateForm2() {
+	if(document.getElementById('addBrandID').value == "") {
+		swal({
+		title: "Warning!",
+		text: "Please Enter Brand ID.",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addBrandID').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addBrandID').value.length < 3){
+		swal({
+		title: "Warning!",
+		text: "The Brand ID should atleast have 3 characters",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addBrandID').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addBrandName').value == "") {
+		swal({
+		title: "Warning!",
+		text: "Please Enter Brand Name.",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addBrandName').style.borderColor = "red";
+		return false;
+	}
+	if (document.getElementById('addBrandName').value.length < 3){
+		swal({
+		title: "Warning!",
+		text: "The Brand Name should atleast have more than 2 characters",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('addBrandName').style.borderColor = "red";
+		return false;
+	}
+	if(confirm('Are you sure you want to update this entry?')) {
+		return true;
+	}
+	else {
+		swal({
+		title: "Updating of Brand Cancelled",
+		type: "success"
+		});
+		return false;		
+	}
+}
