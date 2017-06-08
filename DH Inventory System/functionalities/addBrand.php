309349f825a391207d0d836c6163fb7e188408a8
Kreezy <Kreezykreez@yahoo.com>
2017-06-08 15:34:00 +0800

    Updated validation for add brand.

--- DH Inventory System/functionalities/addBrand.php
+++ DH Inventory System/functionalities/addBrand.php
@@ -5,7 +5,7 @@
 		
 		$brandID = $_POST['brandID'];
 	    $brandName = $_POST['brandName'];
-		$query = $conn->prepare("Select * FROM brand WHERE brandID = '$brandID' AND brandName = '$brandName'");
+		$query = $conn->prepare("Select * FROM brand WHERE brandID = '$brandID'");
 		$count = $query->execute();
 		$row = $query->fetch();
 
@@ -13,10 +13,29 @@
 			echo '<script language="javascript">';
 			echo 'swal(
 				  "Error!",
-				  "Brand Already Exists, New Brand Has Not been Added",
+				  "Brand ID Already Exists, New Brand Has Not been Added",
 				  "error");';
 			echo '$("#myModal").modal("show");';
-			echo 'document.getElementById("addBrandID").style.borderColor = "red";';
+			echo 'document.getElementById("addBrandName").style.borderColor = "red";';
+			echo '</script>';
+			return false;
+		}
+			
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$brandID = $_POST['brandID'];
+	    $brandName = $_POST['brandName'];
+		$query = $conn->prepare("Select * FROM brand WHERE brandName = '$brandName'");
+		$count = $query->execute();
+		$row = $query->fetch();
+
+		if ($query->rowCount() > 0){
+			echo '<script language="javascript">';
+			echo 'swal(
+				  "Error!",
+				  "Brand Name Already Exists, New Brand Has Not been Added",
+				  "error");';
+			echo '$("#myModal").modal("show");';
 			echo 'document.getElementById("addBrandName").style.borderColor = "red";';
 			echo '</script>';
 		} else {
