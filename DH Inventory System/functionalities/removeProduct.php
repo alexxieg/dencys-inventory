ce7390e531a7bd07bf436074f0c29b6cea2c196d
Alex <alelexxie@gmail.com>
2017-06-08 12:24:56 +0800

    updated remove and restore function for product

--- DH Inventory System/functionalities/removeProduct.php
+++ DH Inventory System/functionalities/removeProduct.php
@@ -5,6 +5,10 @@
     $proid= $_GET['proId'];
     $result = $conn->prepare("UPDATE product SET status = 'Inactive' WHERE prodID = '$proid'");
     $result->execute();
+	
+	$result1 = $conn->prepare("UPDATE product SET archiveDate = CURDATE() WHERE prodID = '$proid'");
+	$result1->execute();
+	
     header("location: ../product.php");
 
 ?>
--- DH Inventory System/functionalities/restoreProduct.php
+++ DH Inventory System/functionalities/restoreProduct.php
@@ -5,6 +5,10 @@
     $proid= $_GET['proId'];
     $result = $conn->prepare("UPDATE product SET status = 'Active' WHERE prodID = '$proid'");
     $result->execute();
+	
+	$result1 = $conn-prepare("UPDATE product SET restoreDate = CURDATE() WHERE prodID = '$proid'");
+	$result1->execute();
+	
     header("location: ../product.php");
 
 ?>
