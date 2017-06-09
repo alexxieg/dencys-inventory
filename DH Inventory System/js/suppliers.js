983d77292010019e552ad9b21d5b16f3f156dcab
Kreezy <KreezyKreez@yahoo.com>
2017-06-09 15:02:47 +0800

    added confirm box and fixed validation.

--- DH Inventory System/js/suppliers.js
+++ DH Inventory System/js/suppliers.js
@@ -1,3 +1,4 @@
+var check;
 function validateForm() {
 	if(document.getElementById('supName').value == "") {
 		swal({
@@ -21,6 +22,17 @@ function validateForm() {
 		return false;
 	}
 	
+	if (document.getElementById('supContact').value.length < 7){
+		swal({
+		title: "Warning!",
+		text: "The contac number should have more than 6 characters",
+		type: "warning",
+		confirmButtonText: "Ok"
+		});
+		document.getElementById('supContact').style.borderColor = "red";
+		return false;
+	}
+	
 	if (document.getElementById('supLoc').value == "") {
 		swal({
 		title: "Warning!",
@@ -32,15 +44,26 @@ function validateForm() {
 		return false;
 	}
 	
-	if(confirm('Are you sure you want to add this supplier?')) {
-		return true;
-	}
-	else {
+	if(check != true) {
+		event.preventDefault();
 		swal({
-		title: "Adding of supplier cancelled",
-		type: "success"
+		  title: '<b>Confirm New Supplier</b>',
+		  type: 'info',
+		  text: "Are you sure you want to add the entry?",
+		  showCloseButton: true,
+		  showCancelButton: true,
+		  confirmButtonText:
+			'<button id="thisButton">YES</button>',
+		  cancelButtonText:
+			'<button>Cancel</button>'
 		});
-		return false;		
+		$('#thisButton').click(function(){
+			check = true;
+			document.getElementById('sucBtn').click();
+		});
+		
+	} else {
+		return true;
 	}
 }
 
