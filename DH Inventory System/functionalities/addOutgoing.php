3eec8dcd32b8de2ee38425b70cf848038af8e46b
Luigi <cheeseylegs@gmail.com>
2017-06-10 12:44:43 +0800

    for product issuance qty limit

--- DH Inventory System/functionalities/addOutgoing.php
+++ DH Inventory System/functionalities/addOutgoing.php
@@ -39,32 +39,50 @@
 					$retrieveItemQty = $conn->prepare("SELECT qty FROM inventory i INNER JOIN product p ON i.prodID = p.prodID WHERE p.prodName = '$prodItem'");
 					$retrieveItemQty->execute();
 					$itemQty = $retrieveItemQty->fetch();
-						$emp1 = $conn->query("SELECT empID AS empA FROM employee WHERE empFirstName = '$emp'");
-						$emp2 = $emp1->fetch(PDO::FETCH_ASSOC);
-						$emp3 = $emp2['empA'];
-						
-						$prod1 = $conn->query("SELECT prodID AS prodA FROM product WHERE prodName = '$prodItem'");
-						$prod2 = $prod1->fetch(PDO::FETCH_ASSOC);
-						$prod3 = $prod2['prodA'];
-						
-						$branch1 = $conn->query("SELECT branchID AS branchA from branch WHERE location = '$branch'");
-						$branch2 = $branch1->fetch(PDO::FETCH_ASSOC);
-						$branch3 = $branch2['branchA'];
-						
+					$emp1 = $conn->query("SELECT empID AS empA FROM employee WHERE empFirstName = '$emp'");
+					$emp2 = $emp1->fetch(PDO::FETCH_ASSOC);
+					$emp3 = $emp2['empA'];
+					
+					$prod1 = $conn->query("SELECT prodID AS prodA FROM product WHERE prodName = '$prodItem'");
+					$prod2 = $prod1->fetch(PDO::FETCH_ASSOC);
+					$prod3 = $prod2['prodA'];
+					
+					$branch1 = $conn->query("SELECT branchID AS branchA from branch WHERE location = '$branch'");
+					$branch2 = $branch1->fetch(PDO::FETCH_ASSOC);
+					$branch3 = $branch2['branchA'];
+					
+					$checkQuant = current($conn->query("SELECT inventory.qty FROM product LEFT JOIN inventory ON product.prodID = inventory.prodID 
+													LEFT JOIN incoming ON product.prodID = incoming.prodID 
+													LEFT JOIN outgoing ON product.prodID = outgoing.prodID
+													WHERE product.status = 'Active' AND product.prodID = '$prod3' 
+													AND prodName = '$prodItem'
+													GROUP BY qty")->fetch());
+					
+					if ($outQty > $checkQuant) {
+						echo '<script language="javascript">';
+						echo 'swal(
+							  "Error!",
+							  "U R issuing too much dumbass for the product: '.$prodItem.'",
+							  "error");';
+						echo '$("#myModal").modal("show");';
+						echo 'document.getElementById("addRcpt").style.borderColor = "red";';
+						echo '</script>';
+					} else {
 						$sql = "INSERT INTO outgoing (outQty, outDate, receiptNo, branchID, empID, prodID, userID)
 						VALUES ('$outQty',CURDATE(),'$prod','$branch3','$emp3','$prod3','$userID')";
 						$result = $conn->query($sql); 
+					}
 				}
 			}
         }
 
-		$role = $_SESSION['sess_role'];
+		/**$role = $_SESSION['sess_role'];
 		if($role == 'admin'){
 			$url='prodIssuance.php';
 			echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';
 		}else{
 			$url='userProdIssuance.php';
 			echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';
-		}
+		}*/
     }
 ?>	
