5851ad13a001b1aa9d5e96999d9d44fcf562e9d0
Luigi <cheeseylegs@gmail.com>
2017-06-10 12:22:59 +0800

    fixed removal on input

--- DH Inventory System/prodIssuance.php
+++ DH Inventory System/prodIssuance.php
@@ -343,8 +343,19 @@
 												<table class="table table-striped" id="dataTable" name="chk">
 													<tbody>
 														<tr>
+															<td>
+																Yea
+															<td>
+															<td>
+																Yea
+															<td>
+															<td>
+																Yea
+															<td>
+														</tr>
+														<tr>
 															<td><input type="checkbox" name="chk"></TD>
-															<td><input type="hidden" value="1" name="num" id="orderdata">1</td>
+															<td><input type="hidden" value="1" name="num" id="orderdata" hidden></td>
 															<td>	
 																<div class="ui-widget">
 																	<input class="thisProduct" id="prod" name="prodItem[]" placeholder="Product Name" required>
