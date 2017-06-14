f97d49409026bcd5af46207b29b1cf1bbbc076e2
Saeran Desu <kreezykreez@yahoo.com>
2017-06-14 14:33:49 +0800

    Fixed date error.

--- DH Inventory System/functionalities/removeBrand.php
+++ DH Inventory System/functionalities/removeBrand.php
@@ -5,7 +5,7 @@
     $result = $conn->prepare("UPDATE brand SET status = 'Inactive' WHERE brandID = '$brandRemoveID'");
     $result->execute();
 
-	$result2 = $conn->prepare("UPDATE branch SET archiveDate = CURDATE() WHERE branchID = '$brandRemoveID'");
+	$result2 = $conn->prepare("UPDATE brand SET archiveDate = CURDATE() WHERE brandID = '$brandRemoveID'");
 	$result2->execute();
 	
     header("location: ../brands.php");
